a5c7258b7d9a3a00eeef821de193862f81e92ace
FadiiBassem <85284328+FadiiBassem@users.noreply.github.com>
2021-06-04 17:39:54 +0200

    Update LCD_Functions.c
    initial Values

--- LCD_Functions.c
+++ LCD_Functions.c
@@ -1,1 +1,17 @@
 
+void LCD_init(void)
+{
+  Serial.begin(9600);
+  SYSCTL_RCGCGPIO_R |= 0X03;
+  
+  GPIO_PORTA_DIR_R |= 0XE0;
+  GPIO_PORTA_DEN_R |= 0XE0;
+
+  GPIO_PORTB_DIR_R |= 0XFF;
+  GPIO_PORTB_DEN_R |= 0XFF;
+
+  LCD_Cmd(0X38);
+  LCD_Cmd(0x06);
+  LCD_Cmd(0X0F);
+  LCD_Cmd(0X01);
+}
